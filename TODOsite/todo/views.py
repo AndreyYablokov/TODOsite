cb15a1ab6d80e18fe0c9f3f6bf8c2602b2ac785a
Andrey Yablokov <AndrewYablokov@yandex.ru>
2021-12-15 16:24:04 +0300

    Improved project API

--- TODOsite/todo/views.py
+++ TODOsite/todo/views.py
@@ -1,14 +1,22 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
 
 from todo.models import Project
 from todo.models import ToDo
 from todo.serializers import ProjectModelSerializer
 from todo.serializers import ToDoModelSerializer
+from todo.filters import ProjectFilter
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class ToDoModelViewSet(ModelViewSet):
