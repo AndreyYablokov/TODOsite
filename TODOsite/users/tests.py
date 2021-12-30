9361a191690ece65ae3aea47e934d670d8c23206
Andrey <AndrewYablokov@yandex.ru>
2021-12-30 13:50:22 +0300

    Added tests for users API (APIRequestFactory)

--- TODOsite/users/tests.py
+++ TODOsite/users/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, force_authenticate
+from rest_framework import status
 
-# Create your tests here.
+from users.views import UserViewSet
+from users.models import User
+
+
+class TestUserViewSet(TestCase):
+
+    def setUp(self):
+        self.admin = User.objects.create_superuser('root', 'root@TODOsite.ru', 'admin123456')
+        self.user = User.objects.create(username='Andrey_Yablokov', password='qwerty123456!',
+                                        first_name='Andrey', last_name='Yablokov', email='Andrey_Yablokov@TODOsite.ru')
+        self.factory = APIRequestFactory()
+
+    def test_get_list(self):
+        request = self.factory.get('/api/users/')
+        view = UserViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_user(self):
+        request = self.factory.get(f'/api/users/')
+        view = UserViewSet.as_view({'get': 'retrieve'})
+        response = view(request, pk=self.user.id)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_admin(self):
+        request = self.factory.patch(f'/api/users/{self.user.id}/', {'first_name': 'Alex'}, format='json')
+        view = UserViewSet.as_view({'patch': 'partial_update'})
+        force_authenticate(request, self.admin)
+        response = view(request, pk=self.user.id)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        request = self.factory.get(f'/api/users/')
+        view = UserViewSet.as_view({'get': 'retrieve'})
+        response = view(request, pk=self.user.id)
+        self.assertEqual(response.data['first_name'], 'Alex')
